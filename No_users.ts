a851e9bf13fd57cb555a001bd80da68f9991a2af
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:34:17 -0800

    Add files via upload

---
+++ No_users.ts
@@ -0,0 +1,14 @@
+// Written by : TAYYABA RAO
+// Date: 18/feb/2024
+
+//array for usernames
+let usernames: string[] = ['admin','Aslam','Arshad','Afzal','Akram'];
+
+// checking user availibility
+if (usernames.length ===0) {
+    console.log("We need to find some users!");
+} else{
+    // removing all usernames from the array
+    usernames = [];
+    console.log("All usersnames have been removed. " + usernames.length);
+}
